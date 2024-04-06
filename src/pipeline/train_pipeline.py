e9bf5b5a2f867e9ec521d7a11394cba8723cc4eb
jayraj2498 <jayraj2498@gmail.com>
2024-04-06 19:59:51 +0530

    dvc methods added

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -21,10 +21,10 @@ from src.components.model_trainer import ModelTrainer
 if __name__ == "__main__": 
     obj=DataIngestion()
     train_data ,test_data =obj.initiate_data_ingestion()
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data ,test_data)   # here we get preprocess data 
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    # data_transformation=DataTransformation()
+    # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data ,test_data)   # here we get preprocess data 
+    # modeltrainer=ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
     
     
     
